e886830df8f1c2086533d9a0121c1fe6e85df998
Jaden Chant <chant1@stolaf.edu>
2021-02-12 12:11:36 -0600

    tested making classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+var start = "start";
+var end = "end";
+var UserAccount = /** @class */ (function () {
+    function UserAccount(fname, lname, id) {
+        this.fname = fname;
+        this.lname = lname;
+        this.id = id;
+    }
+    return UserAccount;
+}());
+console.log(start);
+var fullname = window.prompt("Please enter your first and last name:\n");
+var users = Array();
+users.push(new UserAccount("first", "last", 0));
+console.log(users[0].fname);
+console.log(end);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello");
+const start = "start";
+const end = "end";
+
+interface User {
+    fname: string;
+    lname: string;
+    id: number;
+}
+
+class UserAccount {
+    fname: string;
+    lname: string;
+    id: number;
+
+    constructor(fname: string, lname: string, id: number){
+        this.fname = fname;
+        this.lname = lname;
+        this.id = id;
+    }
+}
+
+
+console.log(start);
+
+
+let fullname = window.prompt("Please enter your first and last name:\n");
+
+let users = Array<UserAccount>();
+
+users.push(new UserAccount("first", "last", 0));
+
+console.log(users[0].fname);
+
+
+console.log(end);
